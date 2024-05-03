134be6dfa8cfa2f2d6b9aac343895a83a8fc034a
nkonovalov1990 <n.konovalov1990@gmail.com>
2024-05-03 17:58:18 +0200

    apr 2024

---
+++ data.js
@@ -0,0 +1,23 @@
+const PLANS = [
+  {
+    title: "Предложения 2023",
+    map: "Project_planning_2023.jpg",
+    old: true,
+  },
+  {
+    title: "Предложения 2024",
+    map: "Proposals_2024.jpg",
+    old: true,
+  },
+  {
+    title: "Предложения 2024 без БРТ",
+    map: "Proposals_2024_without_BRT.jpg",
+    old: true,
+  },
+  {
+    title: "Исходный проект",
+    map: "Source_project.jpg",
+    old: true,
+    default: true,
+  },
+];
